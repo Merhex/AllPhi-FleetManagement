17d2ead482c1a827b755122400baf294c33888d9
Breekweg Axel <breekweg.axel.mid@gmail.com>
2021-01-19 15:31:09 +0100

    Refactor dependency injected services to specific project for reusability

---
+++ FleetManagement.API.Configurations/Services.cs
@@ -0,0 +1,26 @@
+﻿using FleetManagement.BLL.Components;
+using FleetManagement.BLL.Components.Interfaces;
+using FleetManagement.BLL.Validators;
+using FleetManagement.BLL.Validators.Interfaces;
+using FleetManagement.DAL.Repositories;
+using FleetManagement.DAL.Repositories.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FleetManagement.API.Configurations
+{
+    public static class Services
+    {
+        public static IServiceCollection AddRequiredDependenciesInContainer(this IServiceCollection services)
+        {
+            services.AddTransient<IDriverRepository, DriverRepository>();
+            services.AddTransient<IDriverComponent, DriverComponent>();
+            services.AddTransient<IMotorVehicleComponent, MotorVehicleComponent>();
+            services.AddTransient<IMotorVehicleRepository, MotorVehicleRepository>();
+            services.AddTransient<IBelgianNationalNumberValidator, BelgianNationalNumberValidator>();
+            services.AddTransient<IFuelCardRepository, FuelCardRepository>();
+            services.AddTransient<IFuelCardComponent, FuelCardComponent>();
+
+            return services;
+        }
+    }
+}
